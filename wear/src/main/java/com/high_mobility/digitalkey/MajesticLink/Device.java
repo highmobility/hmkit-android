7ac130ed6f00acbc0ecc9377107e4182c2d2a98d
Tõnis Tiganik <ttiganik@gmail.com>
2016-04-13 13:07:54 +0300

    add Device interface

--- wear/src/main/java/com/high_mobility/digitalkey/MajesticLink/Device.java
+++ wear/src/main/java/com/high_mobility/digitalkey/MajesticLink/Device.java
@@ -1,8 +1,11 @@
 package com.high_mobility.digitalkey.MajesticLink;
 
+import com.high_mobility.digitalkey.MajesticLink.Shared.DeviceCertificate;
+
 /**
  * Created by ttiganik on 13/04/16.
  */
 public class Device {
-
+    public DeviceCertificate certificate;
+    public String name;
 }
