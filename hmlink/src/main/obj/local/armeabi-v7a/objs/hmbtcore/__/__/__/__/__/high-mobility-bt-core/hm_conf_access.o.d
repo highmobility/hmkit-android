f5d505273c46e8f82319897360a482df649b9322
Tõnis Tiganik <ttiganik@gmail.com>
2016-05-19 13:06:41 +0300

    remove jni files

--- hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/__/__/__/__/__/high-mobility-bt-core/hm_conf_access.o.d
+++
@@ -1,8 +0,0 @@
-/Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/__/__/__/__/__/high-mobility-bt-core/hm_conf_access.o: \
- /Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/../../../../../high-mobility-bt-core/hm_conf_access.c \
- /Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/../../../../../high-mobility-bt-core/hm_conf_access.h \
- /Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/../../../../../high-mobility-bt-core/hm_conf.h
-
-/Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/../../../../../high-mobility-bt-core/hm_conf_access.h:
-
-/Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/../../../../../high-mobility-bt-core/hm_conf.h:
--- hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_bt_debug_hal.o.d
+++
@@ -1,5 +0,0 @@
-/Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_bt_debug_hal.o: \
- /Users/ttiganik/Documents/workspace/Android/android-wear-digital-key/hmlink/src/main/jni/hm_bt_debug_hal.c \
- ../../../../../high-mobility-bt-core/hm_bt_debug_hal.h
-
-../../../../../high-mobility-bt-core/hm_bt_debug_hal.h:
