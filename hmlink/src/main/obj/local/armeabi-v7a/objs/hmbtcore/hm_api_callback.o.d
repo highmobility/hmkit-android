8253dc50ea5a859decfdfe26e291f731a0078e87
Maidu Ule <maidu@high-mobility.com>
2017-04-25 10:11:43 +0300

    Updated core

--- hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_api_callback.o.d
+++ hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_api_callback.o.d
@@ -1,12 +1,15 @@
 /Users/maiduule/Workspace/android-sdk/hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_api_callback.o: \
   /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hm_api_callback.c \
   ../../../../high-mobility-bt-core/hm_api_callback.h \
+  ../../../../high-mobility-bt-core/hm_bt_core.h \
   ../../../../high-mobility-bt-core/hm_api.h \
   ../../../../high-mobility-bt-core/hm_cert.h \
   /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hm_config.h \
   /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hmbtcore.h
 
 ../../../../high-mobility-bt-core/hm_api_callback.h:
+
+../../../../high-mobility-bt-core/hm_bt_core.h:
 
 ../../../../high-mobility-bt-core/hm_api.h:
 
--- hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_bt_persistence_hal.o.d
+++ hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_bt_persistence_hal.o.d
@@ -1,10 +1,16 @@
 /Users/maiduule/Workspace/android-sdk/hmlink/src/main/obj/local/armeabi-v7a/objs/hmbtcore/hm_bt_persistence_hal.o: \
   /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hm_bt_persistence_hal.c \
   ../../../../high-mobility-bt-core/hm_bt_persistence_hal.h \
+  ../../../../high-mobility-bt-core/hm_bt_core.h \
+  ../../../../high-mobility-bt-core/hm_api.h \
   /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hmbtcore.h \
   ../../../../high-mobility-bt-core/hm_bt_debug_hal.h
 
 ../../../../high-mobility-bt-core/hm_bt_persistence_hal.h:
+
+../../../../high-mobility-bt-core/hm_bt_core.h:
+
+../../../../high-mobility-bt-core/hm_api.h:
 
 /Users/maiduule/Workspace/android-sdk/hmlink/src/main/jni/hmbtcore.h:
 
