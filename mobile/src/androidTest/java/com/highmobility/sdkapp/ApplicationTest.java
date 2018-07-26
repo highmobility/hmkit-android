305601e4158a067d1bc6364f001eb5ca860efa23
Tõnis Tiganik <ttiganik@high-mobility.com>
2018-07-26 09:26:06 +0300

    use ext variables

--- mobile/src/androidTest/java/com/highmobility/sdkapp/ApplicationTest.java
+++ mobile/src/androidTest/java/com/highmobility/sdkapp/ApplicationTest.java
@@ -1,13 +1,11 @@
 package com.highmobility.sdkapp;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.filters.SmallTest;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@SmallTest
+public class ApplicationTest  {
+    @Test
+    public void logHistory_ParcelableWriteRead() {
+
     }
 }
