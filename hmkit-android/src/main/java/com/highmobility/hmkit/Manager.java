61ecb3ed6af4de4f1e1cb212f445db01908a5647
Tõnis Tiganik <ttiganik@high-mobility.com>
2018-09-27 11:38:16 +0300

    add HMKit.getInstance to deprecated Manager

--- hmkit-android/src/main/java/com/highmobility/hmkit/Manager.java
+++ hmkit-android/src/main/java/com/highmobility/hmkit/Manager.java
@@ -5,5 +5,14 @@ package com.highmobility.hmkit;
  */
 @Deprecated
 public class Manager {
+    /**
+     * @return The HMKit instance.
+     * @deprecated use {@link HMKit#getInstance()} instead.
+     */
+    @Deprecated
+    public static HMKit getInstance() {
+        return HMKit.getInstance();
+    }
 
+    private Manager() {}
 }
