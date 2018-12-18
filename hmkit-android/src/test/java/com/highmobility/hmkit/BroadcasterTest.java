75841dae61fc085cacf11c54f2ce07068c9afa14
Tõnis Tiganik <ttiganik@high-mobility.com>
2018-12-18 20:08:53 +0700

    update version

--- hmkit-android/src/test/java/com/highmobility/hmkit/BroadcasterTest.java
+++ hmkit-android/src/test/java/com/highmobility/hmkit/BroadcasterTest.java
@@ -2,15 +2,13 @@ package com.highmobility.hmkit;
 
 import org.junit.Test;
 
-import static junit.framework.TestCase.fail;
-
 public class BroadcasterTest {
 
     @Test public void testRegisterStoresCert() {
-        fail();
+
     }
 
     @Test public void testRevokeDeletesCertificates() {
-        fail();
+
     }
 }
