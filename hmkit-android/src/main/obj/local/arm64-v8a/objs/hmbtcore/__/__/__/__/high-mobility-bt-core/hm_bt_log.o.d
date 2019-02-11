beca090b8530743c9e289f643190e3fca51f120b
Maidu Üle <maidu@high-mobility.com>
2019-02-11 10:43:56 +0200

    Added extra buffer clean after disconnect and after connect

--- hmkit-android/src/main/obj/local/arm64-v8a/objs/hmbtcore/__/__/__/__/high-mobility-bt-core/hm_bt_log.o.d
+++ hmkit-android/src/main/obj/local/arm64-v8a/objs/hmbtcore/__/__/__/__/high-mobility-bt-core/hm_bt_log.o.d
@@ -1,11 +1,11 @@
-/Users/maiduule/Workspace/hmkit-android/hmkit-android/src/main/obj/local/arm64-v8a/objs/hmbtcore/__/__/__/__/high-mobility-bt-core/hm_bt_log.o: \
-  /Users/maiduule/Workspace/hmkit-android/hmkit-android/src/main/jni/../../../../high-mobility-bt-core/hm_bt_log.c \
+/Users/maidu/Workspace/hmkit-android/hmkit-android/src/main/obj/local/arm64-v8a/objs/hmbtcore/__/__/__/__/high-mobility-bt-core/hm_bt_log.o: \
+  /Users/maidu/Workspace/hmkit-android/hmkit-android/src/main/jni/../../../../high-mobility-bt-core/hm_bt_log.c \
   ../../../../high-mobility-bt-core/hm_bt_log.h \
   ../../../../high-mobility-bt-core/hm_bt_debug_hal.h \
-  /Users/maiduule/Workspace/hmkit-android/hmkit-android/src/main/jni/hm_config.h
+  /Users/maidu/Workspace/hmkit-android/hmkit-android/src/main/jni/hm_config.h
 
 ../../../../high-mobility-bt-core/hm_bt_log.h:
 
 ../../../../high-mobility-bt-core/hm_bt_debug_hal.h:
 
-/Users/maiduule/Workspace/hmkit-android/hmkit-android/src/main/jni/hm_config.h:
+/Users/maidu/Workspace/hmkit-android/hmkit-android/src/main/jni/hm_config.h:
