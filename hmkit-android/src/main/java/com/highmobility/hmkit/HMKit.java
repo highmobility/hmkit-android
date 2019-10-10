db95ec7369b9eb82954b48b83535ec83159702ec
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-10-10 14:05:11 +0700

    fix leaked ble intent receiver

--- hmkit-android/src/main/java/com/highmobility/hmkit/HMKit.java
+++ hmkit-android/src/main/java/com/highmobility/hmkit/HMKit.java
@@ -192,7 +192,7 @@ public class HMKit {
      * Initialise the SDK with context to get access to storage only. Call {@link
      * #setDeviceCertificate (DeviceCertificate, PrivateKey, PublicKey)} later to send Commands.
      *
-     * @param context The application context.
+     * @param context The context.
      * @return The HMKit instance.
      */
     public HMKit initialise(Context context) {
@@ -493,7 +493,7 @@ public class HMKit {
      * Delete an access certificate.
      *
      * @param serial  The serial of the device that is gaining access.
-     * @param context The application context.
+     * @param context The context.
      * @return true if the certificate existed and was deleted successfully, otherwise false.
      * @deprecated Use {@link #deleteCertificate(DeviceSerial)} instead.
      */
@@ -506,7 +506,7 @@ public class HMKit {
     }
 
     /**
-     * @param context The application context.
+     * @param context The context.
      * @param serial  The serial of the device that is providing access (eg this device).
      * @return All stored Access Certificates where the device with the given serial is providing
      * access.
@@ -525,7 +525,7 @@ public class HMKit {
      * Find an Access Certificate with the given serial number.
      *
      * @param serial  The serial number of the device that is gaining access.
-     * @param context The application context.
+     * @param context The context.
      * @return An Access Certificate for the given serial if one exists, otherwise null.
      * @deprecated Use {@link #getStorage()#getCertificate(DeviceSerial)} instead.
      */
@@ -541,7 +541,7 @@ public class HMKit {
     /**
      * Deletes all of the stored Access Certificates.
      *
-     * @param context The application context.
+     * @param context The context.
      * @deprecated Use {@link #getStorage()#deleteCertificates()} instead.
      */
     @Deprecated
@@ -576,7 +576,7 @@ public class HMKit {
             threadManager = new ThreadManager(this.context);
 
             try {
-                ble = new SharedBle(context);
+                ble = new SharedBle(this.context);
             } catch (BleNotSupportedException e) {
                 i("BLE not supported");
             }
