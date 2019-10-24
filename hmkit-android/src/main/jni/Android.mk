cfd0824692a7a8739dd18249ca7e2b8dcea49f04
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-10-24 11:50:18 +0700

    move bt core to jni submodule

--- hmkit-android/src/main/jni/Android.mk
+++ hmkit-android/src/main/jni/Android.mk
@@ -1,4 +1,5 @@
-LOCAL_PATH := ../../../../hm-java-core-jni/src/main/jni
+CORE_PATH := ../../../../hm-java-core-jni
+LOCAL_PATH := $(CORE_PATH)/src/main/jni
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := crypto
@@ -17,8 +18,8 @@ LOCAL_MODULE := hmbtcore
 
 LOCAL_LDLIBS := -llog
 LOCAL_LDLIBS += -lz
-LOCAL_C_INCLUDES += ../../../../high-mobility-bt-core
-LOCAL_C_INCLUDES += ../../../../crypto-c/Crypto
+LOCAL_C_INCLUDES += $(CORE_PATH)/high-mobility-bt-core
+LOCAL_C_INCLUDES += $(CORE_PATH)/crypto-c/Crypto
 
 LOCAL_SRC_FILES := $(LOCAL_PATH)/hmbtcore.cpp
 LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_bt_crypto_hal.c
@@ -27,13 +28,13 @@ LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_connectivity_hal.c
 LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_bt_persistence_hal.c
 LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_api_callback.c
 
-LOCAL_SRC_FILES += ../../../../crypto-c/Crypto/Crypto.c
+LOCAL_SRC_FILES += $(CORE_PATH)/crypto-c/Crypto/Crypto.c
 
-LOCAL_SRC_FILES += ../../../../high-mobility-bt-core/hm_bt_core.c
-LOCAL_SRC_FILES += ../../../../high-mobility-bt-core/hm_bt_log.c
-LOCAL_SRC_FILES += ../../../../high-mobility-bt-core/hm_cert.c
-LOCAL_SRC_FILES += ../../../../high-mobility-bt-core/hm_conf_access.c
-LOCAL_SRC_FILES += ../../../../high-mobility-bt-core/hm_api.c
+LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_bt_core.c
+LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_bt_log.c
+LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_cert.c
+LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_conf_access.c
+LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_api.c
 
 LOCAL_STATIC_LIBRARIES := crypto
 LOCAL_STATIC_LIBRARIES += ssl
