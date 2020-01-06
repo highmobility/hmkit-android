e777aeeec53ea61538ae34b828764d6a5c057e7f
Tõnis Tiganik <ttiganik@high-mobility.com>
2020-01-06 16:44:12 +0700

    use MIT license

--- hmkit-android/src/androidTest/java/com/highmobility/hmkit/CryptoTest.kt
+++ hmkit-android/src/androidTest/java/com/highmobility/hmkit/CryptoTest.kt
@@ -1,3 +1,26 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2014- High-Mobility GmbH (https://high-mobility.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.highmobility.hmkit;
 
 import androidx.test.core.app.ApplicationProvider
@@ -21,9 +44,9 @@ class CryptoTest {
     @Test fun testSignatures() {
         val crypto = kit.crypto
         var publicKey =
-                PublicKey("***REMOVED***")
+                PublicKey("A5A74048A85AC52A2E41DE5F9554C9CC36B6E3721EE8E8CE9169DC54192D17FD52C3BD1A4AE7F592756C083E17E54B7730965D99B238EB8D33B172DC35E32398")
         val privateKey =
-                PrivateKey("***REMOVED***")
+                PrivateKey("1B8593D0478B9017C2427256AAEE25FF8A4E20EC6611AFE31D52B32CE0BECCA2")
 
         val data = Bytes("0203")
         val sig = crypto.sign(data, privateKey)
@@ -31,7 +54,7 @@ class CryptoTest {
         assertTrue(crypto.verify(data, sig, publicKey))
 
         publicKey =
-                PublicKey("***REMOVED***")
+                PublicKey("A6A74048A85AC52A2E41DE5F9554C9CC36B6E3721EE8E8CE9169DC54192D17FD52C3BD1A4AE7F592756C083E17E54B7730965D99B238EB8D33B172DC35E32398")
         assertTrue(crypto.verify(data, sig, publicKey) == false)
     }
 
