2aadea18bc7422339cad8a6dd55e87fd627d1ebf
Maidu Üle <maidu@high-mobility.com>
2020-01-17 15:00:14 +0200

    Added core and crypto from github

--- hmkit-android/src/main/jni/Android.mk
+++ hmkit-android/src/main/jni/Android.mk
@@ -18,23 +18,23 @@ LOCAL_MODULE := hmbtcore
 
 LOCAL_LDLIBS := -llog
 LOCAL_LDLIBS += -lz
-LOCAL_C_INCLUDES += $(CORE_PATH)/high-mobility-bt-core
-LOCAL_C_INCLUDES += $(CORE_PATH)/crypto-c/Crypto
+LOCAL_C_INCLUDES += $(CORE_PATH)/hmkit-core
+LOCAL_C_INCLUDES += $(CORE_PATH)/hmkit-crypto-c
 
 LOCAL_SRC_FILES := $(LOCAL_PATH)/hmbtcore.cpp
-LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_bt_crypto_hal.c
-LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_bt_debug_hal.c
-LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_connectivity_hal.c
-LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_bt_persistence_hal.c
-LOCAL_SRC_FILES += $(LOCAL_PATH)/hm_api_callback.c
+LOCAL_SRC_FILES += $(LOCAL_PATH)/hmkit_core_crypto_hal.c
+LOCAL_SRC_FILES += $(LOCAL_PATH)/hmkit_core_debug_hal.c
+LOCAL_SRC_FILES += $(LOCAL_PATH)/hmkit_core_connectivity_hal.c
+LOCAL_SRC_FILES += $(LOCAL_PATH)/hmkit_core_persistence_hal.c
+LOCAL_SRC_FILES += $(LOCAL_PATH)/hmkit_core_api_callback.c
 
-LOCAL_SRC_FILES += $(CORE_PATH)/crypto-c/Crypto/Crypto.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-crypto-c/Crypto.c
 
-LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_bt_core.c
-LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_bt_log.c
-LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_cert.c
-LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_conf_access.c
-LOCAL_SRC_FILES += $(CORE_PATH)/high-mobility-bt-core/hm_api.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-core/hmkit_core.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-core/hmkit_core_log.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-core/hmkit_core_cert.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-core/hmkit_core_conf_access.c
+LOCAL_SRC_FILES += $(CORE_PATH)/hmkit-core/hmkit_core_api.c
 
 LOCAL_STATIC_LIBRARIES := crypto
 LOCAL_STATIC_LIBRARIES += ssl
