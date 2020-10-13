0ea2aa8590d8d10a8f2a1401df1d650a7a3d9c54
tonis <tonisives@gmail.com>
2020-10-13 10:03:25 +0700

    add CL property version for releasing

---
+++ release-script/2.0.4.sh
@@ -0,0 +1,6 @@
+./gradlew :hmkit-android:artifactoryPublish -Pversion=2.0.4-dev -Prepo=gradle-dev-local
+./gradlew :hmkit-crypto:artifactoryPublish -Pversion=1.4.6-dev -Prepo=gradle-dev-local
+./gradlew :hmkit-utils:artifactoryPublish -Pversion=1.4.5-dev -Prepo=gradle-dev-local
+./gradlew :hmkit-core-jni:artifactoryPublish -Pversion=1.0.2-dev -Prepo=gradle-dev-local
+./gradlew :command-queue:artifactoryPublish -Pversion=1.1.0-dev -Prepo=gradle-dev-local
+./gradlew :hmkit-auto-api:artifactoryPublish -Pversion=3.12.0-dev -Prepo=gradle-dev-local
