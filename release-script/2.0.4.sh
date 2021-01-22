ace7a163f6ba8a4d56ea15f1f5f01af99c17341a
tonis <tonisives@gmail.com>
2021-01-22 10:04:57 +0700

    set default depLocation to 1 in release

--- release-script/2.0.4.sh
+++ release-script/2.0.4.sh
@@ -1,19 +1,19 @@
 ./gradlew clean build
 
-./gradlew :hmkit-utils:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :hmkit-utils:bintrayUpload -PdepLocation=1
+./gradlew :hmkit-utils:publish -Prepo=gradle-release-local
+#./gradlew :hmkit-utils:bintrayUpload
 
-./gradlew :hmkit-auto-api:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :hmkit-auto-api:bintrayUpload -PdepLocation=1
+./gradlew :hmkit-auto-api:publish -Prepo=gradle-release-local
+#./gradlew :hmkit-auto-api:bintrayUpload
 
-./gradlew :hmkit-core-jni:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :hmkit-core-jni:bintrayUpload -PdepLocation=1
+./gradlew :hmkit-core-jni:publish -Prepo=gradle-release-local
+#./gradlew :hmkit-core-jni:bintrayUpload
 
-./gradlew :hmkit-crypto:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :hmkit-crypto:bintrayUpload -PdepLocation=1
+./gradlew :hmkit-crypto:publish -Prepo=gradle-release-local
+#./gradlew :hmkit-crypto:bintrayUpload
 
-./gradlew :hmkit-android:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :hmkit-android:bintrayUpload -PdepLocation=1
+./gradlew :hmkit-android:publish -Prepo=gradle-release-local
+#./gradlew :hmkit-android:bintrayUpload
 
-./gradlew :command-queue:publish -Prepo=gradle-release-local -PdepLocation=1
-#./gradlew :command-queue:bintrayUpload -PdepLocation=1
+./gradlew :command-queue:publish -Prepo=gradle-release-local
+#./gradlew :command-queue:bintrayUpload
